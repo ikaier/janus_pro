cd43a95e83e6352c922d5e99b7c4648dd13c119b
Joyce Martinez <joycetezanos@gmail.com>
2020-08-26 19:12:25 -0500

    Click event listener added to bookmark icon

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,14 +4,18 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-const unsavedLabel = document.querySelectorAll('#unsaved-label ');
-  unsavedLabel.addEventListener('click', (event) => {
-    unsavedLabel.firstElementChild.style.color = "#FF2021";
-    unsavedLabel.lastElementChild.innerText = 'SAVED';
+const saveProduct = document.querySelectorAll('#unsaved-label');
+  saveProduct.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      item.firstElementChild.style.color = "#FF2021";
+      item.lastElementChild.innerText = 'SAVED';
+  })
 });
 
-const savedLabel = document.querySelectorAll('#saved-label');
-  savedLabel.addEventListener('click', (event) => {
-    savedLabel.firstElementChild.style.color = "#545252";
-    savedLabel.lastElementChild.innerText = 'SAVE';
+const unsaveProduct = document.querySelectorAll('#saved-label');
+  unsaveProduct.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      item.firstElementChild.style.color = "#545252";
+      item.lastElementChild.innerText = 'SAVE';
+  })
 });
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,2 @@
 class Product < ApplicationRecord
-  has_one_attached :photo
 end
