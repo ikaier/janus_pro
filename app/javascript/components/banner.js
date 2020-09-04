cd26ab851d72aac1ea7b9b5477cb6375a03e010d
Joyce Martinez <joycetezanos@gmail.com>
2020-09-04 12:46:07 -0500

    Homepage banner copy changed

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Compare Prices From The Leading Stores", "Save Up To 80%"],
+    strings: ["Compare Prices From The Leading Stores", "Discounts Up To 80%"],
     typeSpeed: 70,
     loop: true
   });
